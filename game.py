755ee60d66018c96bdec0ea4b217c24cbdaa56d8
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2025-01-22 22:28:01 +0800

    first commit

---
+++ game.py
@@ -0,0 +1,49 @@
+from mod_char import *
+from mod_party import Party
+import random, os
+
+os.system("cls")
+
+
+def is_anyone_alive(party: Party):
+    return any(member.is_alive() for member in party.members)
+
+
+running = True
+hero_party = Party()
+heroes = [Swordsman("Alucard"), Archer("Miya"), Wizard("Cyclops"), Healer("Estes")]
+for hero in heroes:
+    print(hero_party.add_member(hero))
+turn = 0
+
+boss = BossMonster()
+
+while running:
+    print("\n")
+    for hero in hero_party.members:
+        if isinstance(hero, Healer):
+            print(hero.heal(random.choice(hero_party.members)))
+        else:
+            print(hero.attack(boss))
+
+        if not boss.is_alive():
+            running = False
+            print("The boss has been defeated!")
+            break
+    if not boss.is_alive():
+        break
+    else:
+        print(f"Boss HP remaining: {boss.health}")
+
+    boss_target = random.choice(hero_party.members)
+    print("\n" + boss.attack(boss_target))
+    if not boss_target.is_alive():
+        print(f"\n{boss_target.name} has died!\n")
+        hero_party.remove_member(boss_target)
+        if not is_anyone_alive(hero_party):
+            print("All heroes have died!")
+            break
+    else:
+        print(f"{boss_target.name}'s HP remaining: {boss_target.health}")
+    input("Press Enter to continue...")
+    os.system("cls")
